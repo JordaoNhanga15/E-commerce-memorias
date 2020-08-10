50f03374f4330f81e5c5b750ea092156edb6ea8a
JordaoNhanga15 <jordaonhanga7@gmail.com>
2020-08-10 03:36:35 +0100

    forEach

--- Script/i.js
+++ Script/i.js
@@ -1,18 +1,22 @@
 (function all(win,doc,even){
+
     let form=doc.querySelector('.form')
     let button=doc.querySelector('.btn_form')
-    let text_box=[...doc.querySelectorAll('.text_box')]
+    let text_box=[...doc.querySelectorAll('.text_box')] /*O objeto NodeList dentro de um array*/ 
+
     button.addEventListener('click',text_no,false)
     function text_no(){
+
         text_box.forEach((value,index,array)=>{
             if(text_box[index].value===""){
-                form.classList.add('no_no')
+                return form.classList.add('no_no')
             }
             else{
                 form.classList.add('button')
                 button.style.display="none"
             }
         })
+
         form.addEventListener('animationend',(event)=>{
             if(event.animationName==='no_no'){ 
                 form.classList.remove('no_no')
